836871a2e4b03230c381768aa5fc1ae7bcf2669e
marcusgardh <58325557+marcusgardh@users.noreply.github.com>
2021-04-15 11:22:20 +0200

    adds area that displays team, also adds api call (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Flex } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
+import TeamDisplay from "./components/TeamDisplay";
 
 function App() {
-  return <div className="App"></div>;
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://run.mocky.io/v3/9118e647-e131-43c7-8668-d99af1abb5a6")
+      .then((res) => setList(res.data.team));
+  }, []);
+
+  return (
+    <Flex height="100vh" align="center">
+      <TeamDisplay list={list} />
+    </Flex>
+  );
 }
 
 export default App;
